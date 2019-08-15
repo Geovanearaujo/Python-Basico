5e3e54eeaa821a0089d3cf7361b62330783dd9f1
Geovanearaujo <43480433+Geovanearaujo@users.noreply.github.com>
2019-08-14 21:17:50 -0300

    inicial commit

---
+++ aula1.py
@@ -0,0 +1,46 @@
+#print('hello world!\tsegundo print'
+
+#nome = 'Geovane'
+#idade = 20
+#altura = 1.78
+#tipo_nome = type(nome)
+#tipo_idade = type(idade)
+#tipo_altura = type(altura)
+
+#print(nome)
+#print(idade)
+#print(tipo_nome)
+#print(tipo_idade)
+#print(altura)
+#print(tipo_altura)
+
+#jeito diferente de concatenar(juntar) uma string
+
+#print(nome, 'tem', idade, 'anos e', altura, 'de altura' )
+#ou o str tranforma o float em  str(strig), e o espaçamento tem que ser manual.
+#print(str(nome) + ' tem ' + str(idade) + ' anos e ' + str(altura) + ' de altura' )
+#usando o input
+
+#nome = input('Escreva seu nome: ')
+#idade = input('Escreva sua idade: ')
+#altura = input ('Escreva sua altura: ')
+#print(nome, 'tem', idade, 'anos e', altura, 'de altura' )
+#print(type(nome), type(idade), type(altura))
+
+#fazendo calculadora
+
+#numero1 = 27
+#numero = 53
+
+#resultado = numero1** (1/2)
+
+'''
+Exercicio : faça um formulario que pergunte  o nome, cpf, idade, altura e telefone e imprima isso em um relatorio organizado.
+'''
+
+Nome = input('Escreva seu Nome: ')
+CPF = input ('Escreva seu CPF: ')
+Idade = input ('Escreva seu  Idade: ')
+Altura  = input ('Escreva seu Altura: ')
+Telefone = input ('Escreva seu telefone: ')
+print(Nome, 'Nome''\n', CPF ,'CPF''\n', Idade,'Idade''\n', Altura,'Altura''\n', 'Telefone', Telefone)
